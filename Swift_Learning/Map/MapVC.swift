f9d120921947d3135069c2f359445ca140ff08bd
lin <2423860442@qq.com>
2022-08-28 10:50:44 +0800

    mapkit简单实用

---
+++ Swift_Learning/Map/MapVC.swift
@@ -0,0 +1,57 @@
+//
+//  MapVC.swift
+//  Swift_Learning
+//
+//  Created by Estim on 2022/8/28.
+//
+
+import UIKit
+import MapKit
+class MapVC: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    lazy var locationM: CLLocationManager = {
+        let locationm = CLLocationManager()
+        locationm.requestWhenInUseAuthorization()
+        return locationm
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.locationM.requestWhenInUseAuthorization()
+        setUpMap()
+    
+    }
+    
+    func setUpMap(){
+        //设置地图样式
+        //    case standard = 0 标准
+        //    case satellite = 1 卫星
+        //    case hybrid = 2 标准+卫星
+        //    @available(iOS 9.0, *)
+        //    case satelliteFlyover = 3 3D立体微信
+        //    @available(iOS 9.0, *)
+        //    case hybridFlyover = 4 3D立体混合
+        //    @available(iOS 11.0, *)
+        //    case mutedStandard = 5 一种强调开发人员数据的模式
+        self.mapView.mapType = .standard
+        
+        //设置地图的控制项
+        //        mapView.isScrollEnabled = false
+        //        mapView.isRotateEnabled = false
+        //        mapView.isZoomEnabled = false
+        //设置显示
+        //建筑物
+        mapView.showsBuildings = true
+        //指南针
+        mapView.showsCompass = true
+       
+        mapView.showsUserLocation = true
+        mapView.showsScale = true
+        mapView.showsTraffic = true
+        //拖动有时没用
+        mapView.userTrackingMode = .followWithHeading
+        
+    }
+    
+}
--- Swift_Learning/ViewController.swift
+++ Swift_Learning/ViewController.swift
@@ -8,17 +8,54 @@
 import UIKit
 import AVFoundation
 class ViewController: UIViewController {
-
+    
+    
+    lazy var tableView : UITableView = {
+        let tableView = UITableView.init(frame: self.view.frame)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    lazy var array:[String] = {
+        let array = ["Map使用"]
+        return array
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white;
+        view.addSubview(self.tableView);
         LocationTool.shared.getCurrentLocation(isOnce: false) { loc, errorMsg in
             if errorMsg==nil {
                 print(loc?.coordinate.latitude as Any)
             }
         }
     }
+    
+    
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.array.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = self.array[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            let vc = MapVC()
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
 }
 
